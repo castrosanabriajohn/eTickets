80beaeb7fb353160517fdd9b5a56cf2286523e19
jcastro97 <jcastro@crnova.com>
2023-01-08 16:15:37 -0600

    Configure DbContext

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Receives an options parameter; pass the options parameter to the base class using base keyword 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
