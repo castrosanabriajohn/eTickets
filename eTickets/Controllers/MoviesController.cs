d5d08db5018608485d854831c417dffabcee668a
jcastro97 <jcastro@crnova.com>
2023-01-08 20:43:33 -0600

    Designing the movie card

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Controllers
@@ -14,8 +15,11 @@ namespace eTickets.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
-            return View();
+            var allMovies = await _context.Movies
+                .Include(m => m.Cinema)
+                .OrderBy(m => m.Name)
+                .ToListAsync();
+            return View(allMovies);
         }
     }
 }
