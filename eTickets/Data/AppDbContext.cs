6c9fba8dc1bc3f00f32c9476da70b1f35b027524
jcastro97 <jcastro@crnova.com>
2023-01-08 18:00:26 -0600

    Adding first migration

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -32,6 +32,7 @@ namespace eTickets.Data
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
     }
 }
