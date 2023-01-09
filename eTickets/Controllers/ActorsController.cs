172d0ec9d67aad7f671899b4255a8b6772bf8cb5
jcastro97 <jcastro@crnova.com>
2023-01-08 18:54:37 -0600

    Adding the ActorsController

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,24 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        // Declare the app Db Context
+        private readonly AppDbContext _context;
+        // Inject the context in the constructor
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // Default Method/ActionResult -> Index() was created
+        // To call this view go to -> Actors/Index
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
