de3a81695e83f927e1b85f3a7b2af035f48bc9f9
jcastro97 <jcastro@crnova.com>
2023-01-09 17:04:20 -0600

    Adding EntityBaseRepository files

---
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    // Generic interface where T type is a class
+    // Other services or repositories to inherit from this repository will need to inherit from IEntityBase
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
