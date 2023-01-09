a25ff4e6184b801fa7c108aa67ccdeb69a49a08e
jcastro97 <jcastro@crnova.com>
2023-01-09 13:18:40 -0600

    Model Validation

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -25,6 +26,16 @@ namespace eTickets.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL, Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            _service.Add(actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -7,11 +7,11 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name = "Profile Picture")]
+        [Display(Name = "Profile Picture"), Required(ErrorMessage = "Profile picture is required")]
         public string ProfilePictureURL { get; set; }
-        [Display(Name = "Full Name")]
+        [Display(Name = "Full Name"), Required(ErrorMessage = "Full name is required"), StringLength(50, MinimumLength = 3, ErrorMessage = "Lenght must be between 3 and 50 characters")]
         public string FullName { get; set; }
-        [Display(Name = "Biography")]
+        [Display(Name = "Biography"), Required(ErrorMessage = "Bio is required"), StringLength(50, MinimumLength = 3, ErrorMessage = "Lenght must be between 3 and 50 characters")]
         public string Bio { get; set; }
         // Relationships
         public List<Actor_Movie> Actors_Movies { get; set; }
