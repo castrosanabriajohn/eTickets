edfb0dd1c57c136bf634ae2d0f25a91cf328360a
jcastro97 <jcastro@crnova.com>
2023-01-09 21:05:05 -0600

    Adding new cinema to database

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -8,11 +8,11 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name = "Cinema Logo")]
+        [Display(Name = "Cinema Logo"), Required(ErrorMessage = "Cinema Logo is required")]
         public string Logo { get; set; }
-        [Display(Name = "Cinema Name")]
+        [Display(Name = "Cinema Name"), Required(ErrorMessage = "Cinema Name is required")]
         public string Name { get; set; }
-        [Display(Name = "Description")]
+        [Display(Name = "Description"), Required(ErrorMessage = "Cinema Description is required")]
         public string Description { get; set; }
         // Relationships 
         public List<Movie> Movies { get; set; }
