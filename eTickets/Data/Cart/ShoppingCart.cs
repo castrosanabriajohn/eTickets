831eb29020a1efce5e449e13c99158d8af8e5b7f
jcastro97 <jcastro@crnova.com>
2023-01-19 19:36:36 -0600

    Creating AddItemToCart() method

--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -11,6 +11,21 @@ namespace eTickets.Data.Cart
         public AppDbContext _context;
         public string ShoppingCartId { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public void AddItemToCart(Movie movie)
+        {
+            ShoppingCartItem shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(spi => spi.Movie.Id == movie.Id);
+            switch (shoppingCartItem)
+            {
+                case null:
+                    shoppingCartItem = new() { ShoppingCartId = ShoppingCartId, Movie = movie, Amount = 1 };
+                    _context.ShoppingCartItems.Add(shoppingCartItem);
+                    break;
+                default:
+                    shoppingCartItem.Amount++;
+                    break;
+            }
+            _context.SaveChanges();
+        }
         public ShoppingCart(AppDbContext context) => _context = context; // Injected instance of DbContext for database communication
         // Methods
         public List<ShoppingCartItem> GetAllShoppingCartItems() => ShoppingCartItems ??= _context.ShoppingCartItems.Where(predicate: spi => spi.ShoppingCartId == ShoppingCartId)
