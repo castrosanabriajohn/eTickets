2517d465396cd9d51bb9589403dc1a04af8844f7
jcastro97 <jcastro@crnova.com>
2023-01-19 20:42:34 -0600

    Adding OrdersController file

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _service;
+        private readonly ShoppingCart _cart;
+        public OrdersController(IMoviesService service, ShoppingCart cart)
+        {
+            _service = service;
+            _cart = cart;
+        }
+        // Index action to retrieve and display list of all shopping cart items
+        public IActionResult Index()
+        {
+            _cart.ShoppingCartItems = _cart.GetAllShoppingCartItems();
+            ShoppingCartVM model = new() { ShoppingCart = _cart, ShoppingCartTotal = _cart.GetShoppingCartTotal() };
+            return View(model: model);
+        }
+    }
+}
