24e458cf9df7253ef7c1c769ad298bc1c77b65da
jcastro97 <jcastro@crnova.com>
2023-01-19 18:04:58 -0600

    Adding Order & OrderItem models

---
+++ eTickets/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        [ForeignKey("MovieId")]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        [ForeignKey("OrderId")]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
