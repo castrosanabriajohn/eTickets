eb40df8414300fde9db34ecc52578fd96af10f63
jcastro97 <jcastro@crnova.com>
2023-01-22 16:27:37 -0600

    ViewComponents

---
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace eTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _cart;
+        public ShoppingCartSummary(ShoppingCart cart) => _cart = cart;
+        public IViewComponentResult Invoke()
+        {
+            List<ShoppingCartItem> shoppingCartItems = _cart.GetAllShoppingCartItems();
+            return View(shoppingCartItems.Count);
+        }
+    }
+}
