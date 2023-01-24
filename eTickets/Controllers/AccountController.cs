539b4231ea36340166ddddb15c13c0aa56f5c1c0
jcastro97 <jcastro@crnova.com>
2023-01-24 13:57:11 -0600

    Logging users

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace eTickets.Controllers
 {
@@ -15,5 +17,27 @@ namespace eTickets.Controllers
                                  SignInManager<ApplicationUser> signInManager,
                                  AppDbContext context) => (_userManager, _signInManager, _context) = (userManager, signInManager, context);
         public IActionResult Login() => View(model: new LoginVM());
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!@ModelState.IsValid) return View(model: loginVM);
+            ApplicationUser user = await _userManager.FindByEmailAsync(email: loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user: user, password: loginVM.Password);
+                if (passwordCheck)
+                {
+                    SignInResult result = await _signInManager.PasswordSignInAsync(user: user, password: loginVM.Password, isPersistent: false, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction(actionName: "Index", controllerName: "Movies");
+                    }
+                    TempData["Error"] = "Wrong credentials, please try again!";
+                    return View(model: loginVM);
+                }
+            }
+            TempData["Error"] = "Wrong credentials, please try again!";
+            return View(model: loginVM);
+        }
     }
 }
--- eTickets/Data/ViewModels/LoginVM.cs
+++ eTickets/Data/ViewModels/LoginVM.cs
@@ -4,8 +4,8 @@ namespace eTickets.Data.ViewModels
 {
     public class LoginVM
     {
-        [Required(ErrorMessage = "Email address is required"), Display(Name = "Email address")]
-        public string EmailAddress { get; set; } /**/
+        [Required(ErrorMessage = "Email address is required"), Display(Name = "Email address"), RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Must be a valid email")]
+        public string EmailAddress { get; set; }
         [Required(ErrorMessage = "Password required"), DataType(DataType.Password), Display(Name = "Password"), RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,15}$", ErrorMessage = "Must be a strong password")]
         public string Password { get; set; }
     }
