111f242200de7bb9f2dc9062f53f7e62c0316a85
jcastro97 <jcastro@crnova.com>
2023-01-24 10:29:19 -0600

    Adding the AccountController

---
+++ eTickets/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using eTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager; // For user related data
+        private readonly SignInManager<ApplicationUser> _signInManager; // To check if user is signed in
+        private readonly AppDbContext _context;
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager,
+                                 AppDbContext context) => (_userManager, _signInManager, _context) = (userManager, signInManager, context);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
